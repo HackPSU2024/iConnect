0d3227fce969c8ae3ad88926ae534fdbeb70ce55
Brian <69675700+chenkuanliao@users.noreply.github.com>
2024-03-16 16:39:05 -0400

    basic framework

--- iConnect-app/App.js
+++ iConnect-app/App.js
@@ -1,19 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Card from './compenents/Card';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Wallet" component={Wallet} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function Home() {
+  return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Card 
+        myName="King Bob"
+        myTitles="Student at PSU"
+        imageUrl={require("./image/fakeImage.png")}
+        myDescription={"hehehe"}
+      />
     </View>
+  );
+}
+
+function Wallet() {
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Card 
+          myName="King Bob"
+          myTitles="Student at PSU"
+          imageUrl={require("./image/fakeImage.png")}
+          myDescription={"hehehe"}
+        />
+        <Card 
+          myName="King Bob"
+          myTitles="Student at PSU"
+          imageUrl={require("./image/fakeImage.png")}
+          myDescription={"hehehe"}
+        />
+        <Card 
+          myName="King Bob"
+          myTitles="Student at PSU"
+          imageUrl={require("./image/fakeImage.png")}
+          myDescription={"hehehe"}
+        />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ iConnect-app/compenents/Card.js
@@ -0,0 +1,58 @@
+// Card.js
+import React from 'react';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+
+const Card = ({ myName, myTitles, imageUrl, myDescription }) => {
+  return (
+    <View style={styles.card}>
+      <Text style={styles.myName}>{myName}</Text>
+      <Text style={styles.myTitles}>{myTitles}</Text>
+      <Image source={imageUrl} style={styles.image} />
+      <Text style={styles.myDescription}>{myDescription}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  card: {
+    width: "80%",
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    padding: 10,
+    marginBottom: 20,
+  },
+  myName: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  myTitles: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  myDescription: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+};
+
+export default Card;
